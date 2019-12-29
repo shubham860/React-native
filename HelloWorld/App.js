d037b5a5d85cfe88789a4aee06043a7cf3bcdfa9
shubham860 <shubham.chauhan860@gmail.com>
2019-12-30 02:53:51 +0530

    commit-4

--- HelloWorld/App.js
+++ HelloWorld/App.js
@@ -3,11 +3,14 @@ import Blinker from './components/Blinker';
 import Styling from './components/Styling';
 import NormalButton from './components/NormalButton'
 import TouchableButtons from './components/TouchableButtons';
+import Scrollview from './components/Scrollview';
+import Flatlist from './components/Flatlist';
+import SectionList from './components/SectionList';
 class App extends Component {
 
     render() {
     return (
-       <TouchableButtons/>
+       <SectionList/>
     );
   }
 }
