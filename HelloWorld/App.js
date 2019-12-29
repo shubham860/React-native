0445fdcd44fb40e26e007716400bdef7f9ff3f6a
shubham860 <shubham.chauhan860@gmail.com>
2019-12-30 02:19:44 +0530

    commit-3

--- HelloWorld/App.js
+++ HelloWorld/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Blinker from './components/Blinker';
 import Styling from './components/Styling';
+import NormalButton from './components/NormalButton'
+import TouchableButtons from './components/TouchableButtons';
 class App extends Component {
 
     render() {
     return (
-       <Styling/>
+       <TouchableButtons/>
     );
   }
 }
